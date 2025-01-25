d8df35903e411e030714de7de04f990cb38c966c
Emerson Clay <136268542+c1ayer@users.noreply.github.com>
2025-01-25 13:49:51 -0600

    various

--- michael-bubble/scenes/character_body_2d.gd
+++ michael-bubble/scenes/character_body_2d.gd
@@ -26,27 +26,3 @@ func _physics_process(delta):
 	get_input(delta)
 	rotation += rotation_direction * rotation_speed * delta
 	move_and_slide()
-
-
-
-
-
-"""
-#this is another type of movement that follows a more traditional up down left right kinda deal.
-
-
-extends CharacterBody2D
-
-@export var speed = 400
-
-func get_input():
-	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = input_direction * speed
-
-func _physics_process(delta):
-	get_input()
-	move_and_slide()
-
-
-
-"""
