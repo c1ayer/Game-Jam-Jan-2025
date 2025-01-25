2b2ac4515de9afd0f2a8dc541707f349cae6f05f
pheltemes <45610016+pheltemes@users.noreply.github.com>
2025-01-25 13:48:13 -0600

    OxgyenBar Updates

--- michael-bubble/scripts/bubble_spawner.gd
+++ michael-bubble/scripts/bubble_spawner.gd
@@ -8,12 +8,15 @@ var MAX_SPAWN_Y = 1000
 var num_bubbles = 0
 var MAX_NUM_BUBBLES = 10
 
+signal increase_oxygen
+
 func _on_timer_timeout() -> void:
 	if (num_bubbles > MAX_NUM_BUBBLES) :
-		pass
+		return
 	var bub = bubble.instantiate()
 	bub.position.x = randf_range(MIN_SPAWN_X, MAX_SPAWN_X)
 	bub.position.y = randf_range(MIN_SPAWN_Y, MAX_SPAWN_Y)
+	bub.connect("player_popped", _on_player_pop_bubble)
 	num_bubbles += 1
 	print("bubbles = " + str(num_bubbles))
 	add_child(bub)
@@ -22,3 +25,7 @@ func _on_timer_timeout() -> void:
 func _on_child_exiting_tree(node: Node) -> void:
 	#if Bubble is removed we decrease active bubble count
 	num_bubbles -= 1
+	
+	
+func _on_player_pop_bubble() :
+	emit_signal("increase_oxygen")
--- michael-bubble/scripts/game_manager.gd
+++ michael-bubble/scripts/game_manager.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
+signal oxygen_bar_increase
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	pass
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	#decrease oxygen
 	pass
+
+func _on_bubble_spawner_increase_oxygen() -> void:
+	emit_signal("oxygen_bar_increase")
+	pass # Replace with function body.
