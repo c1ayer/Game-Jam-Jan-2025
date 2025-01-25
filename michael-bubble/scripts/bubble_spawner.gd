8524fdd6895617dae7e8a58ad202bbbe5a158037
pheltemes <45610016+pheltemes@users.noreply.github.com>
2025-01-25 11:07:05 -0600

    Update Bubble

--- michael-bubble/scripts/bubble_spawner.gd
+++ michael-bubble/scripts/bubble_spawner.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
 @onready var bubble = preload("res://scenes/bubble.tscn")
+var MIN_SPAWN_X = 1
+var MAX_SPAWN_X = 1000
+var MIN_SPAWN_Y = 1
+var MAX_SPAWN_Y = 1000
 
 func _on_timer_timeout() -> void:
 	var bub = bubble.instantiate()
-	bub.position = position
+	bub.position.x = randf_range(MIN_SPAWN_X, MAX_SPAWN_X)
+	bub.position.y = randf_range(MIN_SPAWN_Y, MAX_SPAWN_Y)
 	print("bubble spawned")
 	add_child(bub)
