a465cfbeba4ffdbc25e570ad635cfe4aeee33f4f
pheltemes <45610016+pheltemes@users.noreply.github.com>
2025-01-25 21:27:12 -0600

    Fix Bubble Spawning

--- michael-bubble/scripts/bubble.gd
+++ michael-bubble/scripts/bubble.gd
@@ -8,3 +8,7 @@ func _on_area_2d_area_entered(_area: Area2D) -> void:
 	emit_signal("player_popped")
 	# TODO: Play Animation and Sound
 	queue_free()
+
+func _process(delta: float) -> void:
+	if position.y < -2500:
+		queue_free()
