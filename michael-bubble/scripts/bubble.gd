e8be2626963c5d8fa9a5f6bf63f8931fb46d9972
pheltemes <45610016+pheltemes@users.noreply.github.com>
2025-01-26 11:09:44 -0600

    Update Lighting and Bubble Grab

--- michael-bubble/scripts/bubble.gd
+++ michael-bubble/scripts/bubble.gd
@@ -2,13 +2,13 @@ extends RigidBody2D
 
 signal player_popped
 
-func _on_area_2d_area_entered(_area: Area2D) -> void:
-	print("pop")
-	# TODO: Trigger Oxygen Bump if Body is Player
-	emit_signal("player_popped")
-	# TODO: Play Animation and Sound
-	queue_free()
-
 func _process(delta: float) -> void:
 	if position.y < -2500:
 		queue_free()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("pop")
+	emit_signal("player_popped")
+	# TODO: Play Animation and Sound
+	queue_free()
