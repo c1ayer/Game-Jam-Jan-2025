606724b820530e000fce93509ee722251afbefee
Oliver Clay <olijclay@gmail.com>
2025-01-26 13:44:11 -0600

    fish spin
    fish spin

--- michael-bubble/scenes/tigerfish.gd
+++ michael-bubble/scenes/tigerfish.gd
@@ -7,9 +7,10 @@ var aggro = false
 
 
 func _physics_process(delta: float) -> void:
-	if aggro:
+	if aggro == true:
 		var direction = global_position.direction_to(player.global_position)
 		velocity = direction * 200.0
+		rotation = direction.angle()
 		move_and_slide()
 	
 
